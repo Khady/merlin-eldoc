e5132e05b83f943b68d4be1245426800529cae65
Louis Roché <louis@louisroche.net>
2018-04-27 11:54:24 +0000

    cleanup
    - change mode name
    - group utils
    - make the state more local
    - don't do anything if the point didn't move
    - fix minor mode creation

--- merlin-auto-hl.el
+++ merlin-auto-hl.el
@@ -1,7 +1,22 @@
 (require 'merlin)
 
+(defcustom merlin-auto-hl-idle-time 0.50
+  "How long to wait after user input before highlighting the current identifier."
+  :type 'float
+  :group 'merlin-auto-hl)
+
+(defvar merlin-auto-hl--identifier-timer
+  nil
+  "The timer used for highlighting identifiers.")
+
+(defvar-local merlin-auto-hl--previous-point
+  nil
+  "The value of the point last time this mode displayed information.")
+
+;;; Utils
+
 ;; imported from evil-matchit
-(defun merlin--hl-current-font-among-fonts-p (pos fonts)
+(defun merlin-auto-hl--current-font-among-fonts-p (pos fonts)
   "If current font at POS is among FONTS."
   (let* ((fontfaces (get-text-property pos 'face)))
     (when (not (listp fontfaces))
@@ -11,78 +26,79 @@
                     (member f fonts))
                   fontfaces))))
 
-(defun merlin--hl-in-comment-p (pos)
+(defun merlin-auto-hl--in-comment-p (pos)
   "Check character at POS is comment by comparing font face."
-  (merlin--hl-current-font-among-fonts-p pos '(font-lock-comment-face
-                                               font-lock-comment-delimiter-face)))
+  (merlin-auto-hl--current-font-among-fonts-p pos '(font-lock-comment-face
+                                                    font-lock-comment-delimiter-face)))
 
 
-(defun merlin--hl-in-string-or-doc-p (pos)
+(defun merlin-auto-hl--in-string-or-doc-p (pos)
   "Check character at POS is string or document by comparing font face."
-  (merlin--hl-current-font-among-fonts-p pos '(font-lock-string-face
-                                               font-lock-doc-face)))
+  (merlin-auto-hl--current-font-among-fonts-p pos '(font-lock-string-face
+                                                    font-lock-doc-face)))
 
-(defcustom merlin-hl-identifier-idle-time 0.50
-  "How long to wait after user input before highlighting the current identifier."
-  :type 'float
-  :group 'merlin)
-
-(defvar merlin--hl-identifier-timer
-  nil
-  "The global timer used for highlighting identifiers.")
-
-(defun merlin--hl-type ()
-  (interactive)
-  ;; reset verbosity to avoid diplaying deeper types
-  (setq merlin--verbosity-cache nil)
-  (unless (or (minibufferp)
-              (string-match "*" (buffer-name)))
-    (if (region-active-p)
-        (merlin--type-region)
-      (merlin--type-enclosing-query)
-      (when merlin-enclosing-types
-        (let ((data (elt merlin-enclosing-types merlin-enclosing-offset)))
-          (if (cddr data)
-              (if (merlin--is-short (merlin--type-enclosing-text data))
-                  (merlin--type-display (cdr data) (merlin--type-enclosing-text data))
-                (message "type is too long, check buffer `*merlin-types*' or query the type manually"))))
-        (merlin--type-enclosing-after)))))
-
-(defun merlin--hl-is-keyword-p (pos)
+(defun merlin-auto-hl--is-keyword-p (pos)
   "Check if character at POS is keyword by comparing font face."
-  (merlin--hl-current-font-among-fonts-p pos '(tuareg-font-lock-governing-face
-                                               font-lock-keyword-face)))
+  (merlin-auto-hl--current-font-among-fonts-p pos '(tuareg-font-lock-governing-face
+                                                    font-lock-keyword-face)))
 
-(defun merlin--hl-valid-position-p (pos)
+(defun merlin-auto-hl--valid-position-p (pos)
   "Check if POS is in a place valid to get a type"
-  (and (not (merlin--hl-in-comment-p pos))
-       (or (not (merlin--hl-is-keyword-p pos)) (merlin--hl-in-string-or-doc-p pos))))
+  (and (not (merlin-auto-hl--in-comment-p pos))
+       (or (not (merlin-auto-hl--is-keyword-p pos)) (merlin-auto-hl--in-string-or-doc-p pos))))
 
-(defun merlin--hl-identifiers-function ()
+;;; Main logic
+
+(defun merlin-auto-hl--type ()
+  (interactive)
+  ;; reset verbosity to not display deeper types
+  (setq merlin--verbosity-cache nil)
+  (if (region-active-p)
+      (merlin--type-region)
+    (merlin--type-enclosing-query)
+    (when merlin-enclosing-types
+      (let ((data (elt merlin-enclosing-types merlin-enclosing-offset)))
+        (if (cddr data)
+            (if (merlin--is-short (merlin--type-enclosing-text data))
+                (merlin--type-display (cdr data) (merlin--type-enclosing-text data))
+              (message "type is too long, check buffer `*merlin-types*' or query the type manually"))))
+      (merlin--type-enclosing-after))))
+
+(defun merlin-auto-hl--identifiers-function ()
   "Function run after an idle timeout, highlighting the
 identifier at point, if necessary."
-  (when merlin-hl-identifier-mode
-    (let ((symbol (thing-at-point 'symbol)))
-      (unless (or (not symbol) (not (merlin--hl-valid-position-p (point))))
-        (merlin--hl-type)
-        (unless (eq merlin--current-hl-identifier-idle-time merlin-hl-identifier-idle-time)
-          (merlin--hl-set-timer))))))
+  (when (and merlin-auto-hl-mode                    ; current buffer has the mode on
+             (not (string-match "*" (buffer-name))) ; avoid buffers like `*merlin-types*'
+             (not (minibufferp)))
+    (let ((symbol (thing-at-point 'symbol))
+          (pos (point)))
+      (unless (or (eq pos merlin-auto-hl--previous-point)
+                  (not symbol)
+                  (not (merlin-auto-hl--valid-position-p pos)))
+        (setq-local merlin-auto-hl--previous-point pos)
+        (merlin-auto-hl--type)
+        (unless (eq merlin-auto-hl--current-hl-identifier-idle-time merlin-auto-hl-idle-time)
+          (merlin-auto-hl--set-timer))))))
 
-(defun merlin--hl-set-timer ()
-  (if merlin--hl-identifier-timer
-      (cancel-timer merlin--hl-identifier-timer))
-  (setq merlin--current-hl-identifier-idle-time merlin-hl-identifier-idle-time)
-  (setq merlin--hl-identifier-timer (run-with-idle-timer
-                                     merlin-hl-identifier-idle-time
-                                     t
-                                     #'merlin--hl-identifiers-function)))
+(defun merlin-auto-hl--cancel-timer ()
+  (if merlin-auto-hl--identifier-timer
+      (cancel-timer merlin-auto-hl--identifier-timer)))
 
-(define-minor-mode merlin-hl-identifier-mode
+(defun merlin-auto-hl--set-timer ()
+  (merlin-auto-hl--cancel-timer)
+  (setq merlin-auto-hl--current-hl-identifier-idle-time merlin-auto-hl-idle-time)
+  (setq merlin-auto-hl--identifier-timer (run-with-idle-timer
+                                          merlin-auto-hl-idle-time
+                                          t
+                                          #'merlin-auto-hl--identifiers-function)))
+
+;;;###autoload
+(define-minor-mode merlin-auto-hl-mode
   "Highlight instances of the identifier at point after a short
 timeout."
-  :group 'merlin
-  (if merlin-hl-identifier-mode
-      (unless (or (minibufferp) (string-match "*" (buffer-name)))
-        (merlin--hl-set-timer))))
+  nil nil nil
+  (if merlin-auto-hl-mode
+      (merlin-auto-hl--set-timer)
+    (merlin-auto-hl--cancel-timer)))
 
 (provide 'merlin-auto-hl)
