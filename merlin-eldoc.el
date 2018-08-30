85dec436648f43c050048524fae7a3ad7ad4c019
Louis Roché <louis@louisroche.net>
2018-08-30 10:16:19 +0000

    ignore errors from occurrences detection process (fix #27)

--- merlin-eldoc.el
+++ merlin-eldoc.el
@@ -604,7 +604,7 @@ Occurrences and position are meant to be used by
                        (merlin-eldoc--merlin-error-at-point-p))))
     (merlin-eldoc--adjust-max-len)
     (cond ((merlin-eldoc--valid-type-position-p (point))
-           (merlin-eldoc--highlight-occurrences)
+           (ignore-errors (merlin-eldoc--highlight-occurrences))
            (merlin-eldoc--gather-type-and-doc-info))
           ((merlin-eldoc--valid-fun-args-position-p (point))
            (merlin-eldoc--gather-fun-args))
